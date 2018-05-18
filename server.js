94bdd5e357731f9708aa3d822b20e55a9fdff40e
michelrocha <38973805+michelrocha@users.noreply.github.com>
2018-05-18 15:50:02 +0200

    Video

--- server.js
+++ server.js
@@ -99,6 +99,27 @@ app.get('/heading', function (req, res) {
   }
 });
 
+app.get('/video', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('headings.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('headings.html', { pageCountMessage : null});
+  }
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
